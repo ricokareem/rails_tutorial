0a116c5f72988100480ea77e907348812c2f89f3
Rico Rodriquez Collins <rico.collins@mac.com>
2011-04-30 14:20:14 -0700

    restarting rails tutorial at home

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'c75c65b05e35b93a1b248da195966293f5c718f2b9922ae379e12400fa0fc834670413b483bd6f2f8d6b21a01d388e600a020ba684cdeaf339d57a6fd330ce2a'

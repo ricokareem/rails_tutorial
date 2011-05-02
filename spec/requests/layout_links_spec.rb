a93cb2915550868c5f35806426c3141cb4d09fa3
Rico Rodriquez Collins <rico.collins@mac.com>
2011-05-02 00:06:44 -0700

    add users controller and layout links.

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -20,4 +20,23 @@ describe "LayoutLinks" do
     get '/help'
     response.should have_selector('title', :content => "HELP")
   end
+  
+  it "should have a signup page at '/signup'" do
+    get '/signup'
+    response.shoud have_selector('title', :content => "SIGN UP")
+  end
+  
+  it "should have the right links on layout" do
+    visit root_path
+    response.should have_selector('title', :content => "HOME")
+    click_link "About"
+    response.should have_selector('title', :content => "ABOUT")
+    click_link "Contact"
+    response.should have_selector('title', :content => "CONTACT")
+    click_link "Home"
+    response.should have_selector('title', :content => "HOME")
+    click_link "Sign up now!"
+    response.shoud have_selector('title', :content => "SIGN UP")
+    response.shoud have_selector('a[href="/"]>img)
+  end
 end

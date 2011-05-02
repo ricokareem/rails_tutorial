76fb51c46660d1b082bbcb82012dd417409ace91
Rico Rodriquez Collins <rico.collins@mac.com>
2011-05-01 23:12:42 -0700

    add layout links with routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,19 @@
 class PagesController < ApplicationController
   
   def home
-    @title = "::HOME"
+    @title = "HOME"
   end
 
   def contact
-    @title = "::CONTACT"
+    @title = "CONTACT"
   end
 
   def about
-    @title = "::ABOUT"
+    @title = "ABOUT"
+  end  
+
+  def help
+    @title = "HELP"
   end  
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,7 +6,12 @@ module ApplicationHelper
     if @title.nil?
       base_title
     else
-      "#{base_title}#{@title}"
+      "#{base_title}::#{@title}"
     end
   end
+
+  def logo
+    image_tag("logo.png", :alt => "Sample App", :class => "round")
+  end
+    
 end

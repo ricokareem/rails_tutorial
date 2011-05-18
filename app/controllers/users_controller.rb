98388a62f9e371b6df24ef443757b1c7747d2c5c
Rico Rodriquez Collins <rico.collins@mac.com>
2011-05-18 01:05:35 -0700

    finish user sign up page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,17 @@ class UsersController < ApplicationController
     @title = "Sign Up"
   end
 
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      # one way to do it
+      # flash[:success] = "Welcome to the jungle"
+      # another way to do it
+      redirect_to user_path(@user), :flash => { :success => "Welcome to the jungle"}
+    else    
+      @title = "Sign up"
+      render 'new'
+    end  
+  end  
+  
 end

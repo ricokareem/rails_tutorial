55afb9229e96b70a2c028b2c8449df89076a68bf
Rico Rodriquez Collins <rico.collins@mac.com>
2011-05-22 11:57:31 -0700

    add sessions controller and sign in veiw

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -3,40 +3,45 @@ require 'spec_helper'
 describe "LayoutLinks" do
   it "should have a Home page at '/'" do
      get '/'
-     response.should have_selector('title', :content => "HOME")
+     response.should have_selector('title', :content => "Home")
    end
    
   it "should have a Contact page at '/contact'" do
     get '/contact'
-    response.should have_selector('title', :content => "CONTACT")
+    response.should have_selector('title', :content => "Contact")
   end
   
   it "should have an About page at '/about'" do
     get '/about'
-    response.should have_selector('title', :content => "ABOUT")
+    response.should have_selector('title', :content => "About")
   end
   
   it "should have a Help page at '/help" do
     get '/help'
-    response.should have_selector('title', :content => "HELP")
+    response.should have_selector('title', :content => "Help")
   end
   
   it "should have a signup page at '/signup'" do
     get '/signup'
-    response.shoud have_selector('title', :content => "SIGN UP")
+    response.shoud have_selector('title', :content => "Sign Up")
+  end
+  
+  it "should have a sign-in page at '/signin'" do
+    get '/signin'
+    response.shoud have_selector('title', :content => "Sign in")
   end
   
   it "should have the right links on layout" do
     visit root_path
-    response.should have_selector('title', :content => "HOME")
+    response.should have_selector('title', :content => "Home")
     click_link "About"
-    response.should have_selector('title', :content => "ABOUT")
+    response.should have_selector('title', :content => "About")
     click_link "Contact"
-    response.should have_selector('title', :content => "CONTACT")
+    response.should have_selector('title', :content => "Contact")
     click_link "Home"
-    response.should have_selector('title', :content => "HOME")
+    response.should have_selector('title', :content => "Home")
     click_link "Sign up now!"
-    response.shoud have_selector('title', :content => "SIGN UP")
+    response.shoud have_selector('title', :content => "Sign Up")
     response.shoud have_selector('a[href="/"]>img)
   end
 end

663e617c0690fa35aeb5dc2bd8cf8c12c1ceaed1
Rico Rodriquez Collins <rico.collins@mac.com>
2011-05-23 22:36:47 -0700

    handle invalid sign in

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -4,6 +4,7 @@ describe SessionsController do
   render_views
 
   describe "GET 'new'" do
+    
     it "should be successful" do
       get :new
       response.should be_success
@@ -13,10 +14,32 @@ describe SessionsController do
       get :new
       response.should have_selector('title', :content => "Sign in")
     end
-
-
-
   end
 
+  describe "Post 'create'" do
+    
+    describe "failure" do
+      
+      before(:each) do
+        @attr = { :email => "", :password =>"" }
+      end
+      
+      it "should render the /new page" do
+        post :create, :session => @attr
+        response.should render_template('new')
+      end
+      
+      it "should have the right title" do
+        post :create, :session => @attr
+        response.should have_selector('title', :content => 'Sign in')
+      end
+      
+      it "should have an error message" do
+        post :create, :session => @attr
+        flash.now[:error].should =~ /invalid/i
+      end
+    end
+    
+  end
 
 end

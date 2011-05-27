a7679a40ce279df2e962ddb7e28d28d98959c030
Rico Rodriquez Collins <rico.collins@mac.com>
2011-05-27 01:16:19 -0700

    working user sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+
   def new
     @title = "Sign in"
   end
@@ -11,7 +12,9 @@ class SessionsController < ApplicationController
       @title = "Sign in"
       render 'new'
     else
-      #Handle it
+      sign_in user
+      redirect_to user
+      # redirect_to user_path
     end 
   end
   
